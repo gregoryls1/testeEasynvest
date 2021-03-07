f11a7d194590cd85862c4a7b317d0d2839439cb0
Gregory Lima <contatogregory@gmail.com>
2021-03-07 18:40:54 -0300

    adiciona configuraçao e estrutura do projeto

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+// PLUGIN
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      minify: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      }
+    ]
+  }
+};
+module.exports = config;
