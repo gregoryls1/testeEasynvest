33901379e86c27a089e26179e66d1687f48f704e
Gregory Lima <contatogregory@gmail.com>
2021-03-07 18:44:00 -0300

    correçao da const devMode

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV === 'development';
 
 let config = {
   entry: './src/index.js',
